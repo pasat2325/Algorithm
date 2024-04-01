cf0df044e08ff6f22e8940e1d6325def1d539dd8
김은수 <60413453+pasat2325@users.noreply.github.com>
2024-04-01 21:16:41 +0900

    [Silver III] Title: 소수 구하기, Time: 188 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -6,12 +6,14 @@ using namespace std;
 //소수를 판별하는 함수
 //소수는 1과 자기 자신만을 약수로 가지는 수임
 
+int m, n;
+
 bool isPrime(int data) {
 
 	//1과 2는 for문을 들어가지 않는다
-	if (data == 2) return true;
+
 	if (data == 1) return false;
-	for (int i = 2; i <= sqrt(data) + 1; i++) // data를 2부터 루트(data) 까지 나눴을 때 약수가 존재한다면 -> 소수가 아니다. 
+	for (int i = 2; i * i <= data; i++) // data를 2부터 루트(data) 까지 나눴을 때 약수가 존재한다면 -> 소수가 아니다. 
 	{	
 		if (data % i == 0) return false; //나눠떨어지면 -> 약수이다 -> 소수가 아니다.
 	}
@@ -20,7 +22,6 @@ bool isPrime(int data) {
 
 int main() {
 	
-	int m, n;
 	cin >> m >> n;
 
 	for (int i = m; i < n + 1; i++)
