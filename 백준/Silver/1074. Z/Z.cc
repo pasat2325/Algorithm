af6cb4d8232e818429b1c03e6328777d2e94c84a
김은수 <60413453+pasat2325@users.noreply.github.com>
2024-05-12 17:12:30 +0900

    [Silver I] Title: Z, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int n, r, c;
+int ans = 0;
+
+void dc(int x, int y, int size)
+{
+	if (c == x && r == y) //찾으려는 열과 행 일치
+	{
+		cout << ans;
+		return;
+	}
+	else if (c < x + size && r < y + size && c >= x && r >= y)
+	{
+		dc(x, y, size / 2);
+		dc(x + size / 2, y, size / 2);
+		dc(x, y + size / 2, size / 2);
+		dc(x + size / 2, y + size / 2, size / 2);
+	}
+	else {
+		ans += size * size;
+
+	}
+}
+
+int main()
+{
+	cin >> n >> r >> c;
+	dc(0, 0, pow(2, n));
+	return 0;
+}
