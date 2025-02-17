0453e174e4f49a913b5ee78d73bf9d3057f0f413
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-02-17 11:15:39 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	string expression;
+	stack<char>stack;
+	cin >> expression;
+	expression = '(' + expression + ')';
+
+	for (int i = 0; i < expression.length(); i++)
+	{
+		if (expression[i] < 65 || expression[i] > 90) {
+			if (expression[i] == '*' || expression[i] == '/')
+			{
+				if (stack.top() == '*' || stack.top() == '/')
+				{
+					cout << stack.top();
+					stack.pop();
+				}
+				stack.push(expression[i]);
+			}
+			else if (expression[i] == '+' || expression[i] == '-')
+			{
+				if (stack.top() == '*' || stack.top() == '/')
+				{
+					while (!stack.empty())
+					{
+						if (stack.top() == '(')
+						{
+							break;
+						}
+						else
+						{
+							cout << stack.top();
+							stack.pop();
+						}
+					}
+				}
+				else if (stack.top() == '+' || stack.top() == '-')
+				{
+					cout << stack.top();
+					stack.pop();
+				}
+				stack.push(expression[i]);
+			}
+			else if (expression[i] == ')')
+			{
+				while (!stack.empty())
+				{
+					if (stack.top() == '(')
+					{
+						stack.pop();
+						break;
+					}
+					else
+					{
+						cout << stack.top();
+						stack.pop();
+					}
+				}
+			}
+			else stack.push(expression[i]);
+		}
+		else {
+			cout << expression[i];
+		}
+	}
+}
