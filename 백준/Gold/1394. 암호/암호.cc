8ca6b27ebb8d481b81450c6d52b7a3fa296f3ff4
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-02-24 14:01:41 +0900

    [Gold V] Title: 암호, Time: 8 ms, Memory: 3684 KB -BaekjoonHub

--- 백준/Gold/1394. 암호/암호.cc
+++ 백준/Gold/1394. 암호/암호.cc
@@ -1,42 +1,18 @@
-/*
-* b = 1, c = 2, a = 3으로 매핑.
-* 암호문 bca 순서 = bc 순서 * 암호의 길이 + a의 순서
-* 암호문 bc 순서 = b 순서 * 암호의 길이 + c의 순서
-* 암호문 b 순서 = 1
-* 
-* 암호문의 마지막 글자의 순서를 찾을 때, 순차 탐색 시 최대 1,000,000 * 100번 탐색.
-* map에 인덱스로 저장.	
-* 
-* 암호로 사용 가능한 문자를 1부터 순서대로 매핑. (char,int)
-*/
 #include <iostream>
-#include <unordered_map>
-#include <string>
+#define MOD 900528
 using namespace std;
 
-unordered_map<char, int> map;
-
-string str1, str2;
-
-int solve(int index, int n)
-{
-	if (index == 0) return map[str2[index]];
-
-	return (solve(index - 1, n) * n + map[str2[index]]) % 900528;
-}
+string s;
+long long arr[256], ans, k;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> str1 >> str2;
-
-	for (int i = 1; i <= str1.length(); i++)
-	{
-		map.insert({ str1[i - 1], i});
-	}
-
-	int n = str2.length();
-	cout << solve(str2.length() - 1, str1.length());
-
+	cin >> s;
+	k = s.size();
+	for (int i = 0; i < k; i++) arr[s[i]] = i + 1;
+	cin >> s;
+	for (char e : s) ans = (ans * k + arr[e]) % MOD;
+	cout << ans;
 }
