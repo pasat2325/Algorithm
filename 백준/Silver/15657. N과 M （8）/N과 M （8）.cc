872a5aeac30a9ca37f371fcc9f49d7629e08f689
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-03-02 22:59:04 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m;
+int arr[20];
+int ans;
+vector<int>vec;
+void re(int j, int depth)
+{
+	if (depth == m) {
+		for (int i = 0; i < m; i++) cout << vec[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = j; i < n; i++)
+	{
+		vec.push_back(arr[i]);
+		re(i, depth + 1);
+		vec.pop_back();
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	re(0,0);
+
+}
