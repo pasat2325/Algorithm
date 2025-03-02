493040432b6c4072de0ce6e808250600cf62a9f2
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-03-02 22:32:46 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -4,24 +4,18 @@
 using namespace std;
 int n, m;
 int arr[20];
-vector<int> vec;
 int ans;
 void re(int j, int sum)
 {
-	if (sum == m && j != 0)
-	{
-		ans++;
-	}
+	if (sum == m && j != 0) ans++;
 
-	if (j == n) return;
 	
 	for (int i = j; i < n; i++)
 	{
-		vec.push_back(arr[i]);
+
 		sum += arr[i];
 		re(i + 1, sum);
 		sum -= arr[i];
-		vec.pop_back();
 
 	}
 }
