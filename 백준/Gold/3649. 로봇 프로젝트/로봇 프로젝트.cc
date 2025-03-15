87e088cc41b0c884f42ea91c428745444a13e609
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-03-15 10:15:14 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 1040 ms, Memory: 6048 KB -BaekjoonHub

---
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int x, n;
+	while (cin >> x >> n) {
+		vector<int>v(n);
+		for (int i = 0; i < n; i++) cin >> v[i];
+		sort(v.begin(), v.end());
+		int st = 0, lt = v.size() - 1;
+		if (st == lt) {
+			cout << "danger" << "\n";
+			continue;
+		}
+		int t = x * 10000000;
+		bool flag = false;
+		while (st < lt) {
+			int sum = v[st] + v[lt];
+			if (sum == t) {
+				flag = true;
+				break;
+			}
+			else if (sum > t) lt--;
+			else if (sum < t) st++;
+		}
+		if (!flag) cout << "danger" << "\n";
+		else cout << "yes " << v[st] <<" " <<  v[lt] << "\n";
+	}
+}
