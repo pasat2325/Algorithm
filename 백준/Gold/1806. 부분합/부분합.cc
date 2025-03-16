194166f385fdfb793622d18a067a6857f8d19a43
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-03-16 14:43:59 +0900

    [Gold IV] Title: 부분합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+	int n, s;
+	cin >> n >> s;
+	vector<int>vec(n);
+	for (int i = 0; i < n; i++) {
+		cin >> vec[i];
+	}
+	int start = 0, end = 0;
+	int sum = vec[0];
+	int len = n + 1;
+	while (start <= end && end < n) {
+		if (sum < s) {
+			end++;
+			if (end == n) break;
+			sum += vec[end];
+		}
+		else if (sum >= s) {
+			len = len > end - start + 1 ? end - start + 1 : len;
+			sum -= vec[start];
+			start++;
+		}
+	}
+	if (len == n + 1) cout << 0;
+	else cout << len;
+}
