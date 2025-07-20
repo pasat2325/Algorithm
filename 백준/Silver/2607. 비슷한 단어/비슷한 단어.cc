cc5eb9d00b99dbf16df932c79ae37597fb21e2fb
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-07-20 14:59:15 +0900

    [Silver II] Title: 비슷한 단어, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.cc
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.cc
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-
-bool check(int org_n[], int com_n[]) {
+bool check(int org_n[], int com_n[], bool f) {
+	int diff_sum = 0;
 	for (int i = 0; i < 26; i++) {
-		if (org_n[i] != com_n[i]) return false;
+		diff_sum += abs(org_n[i] - com_n[i]);
 	}
-	return true;
+	if (f) {
+		if (diff_sum == 1) return true;
+		else return false;
+	}
+	else {
+		if (diff_sum == 2 || diff_sum == 0) return true;
+		else return false;
+	}
 }
 int main() {
 	int n, ans = 0;
@@ -14,55 +21,26 @@ int main() {
 	string com;
 	int org_n[26] = { 0 };
 	int com_n[26] = { 0 };
-	int org_length;
-	int com_length;
 	cin >> n >> org;
-	org_length = org.length();
+	int org_length = org.length();
 	for (int i = 0; i < org_length; i++) {
 		org_n[org[i] - 'A']++;
 	}
-	for (int d = 0; d < n - 1; d++) {
+	while (--n) {
+		bool vist = false;
 		cin >> com;
-		com_length = com.length();
+		int com_length = com.length();
 		memset(com_n, 0, sizeof(com_n));
 		for (int i = 0; i < com_length; i++) {
 			com_n[com[i] - 'A']++;
 		}
-		bool vist = false;
-		if (com_length == org_length + 1) {
-			for (int i = 0; i < 26; i++) {
-				org_n[i]++;
-				vist = check(org_n, com_n);
-				org_n[i]--;
-				if (vist) break;
-			}
+		if (org_length == com_length) {
+			vist = check(org_n, com_n, 0);
 		}
-		else if (com_length == org_length - 1) {
-			for (int i = 0; i < 26; i++) {
-				org_n[i]--;
-				vist = check(org_n, com_n);
-				org_n[i]++;
-				if (vist) break;
-			}
+		else if (abs(org_length - com_length) == 1) {
+			vist = check(org_n, com_n, 1);
 		}
-		else if (com_length == org_length) {
-			for (int i = 0; i < 26; i++) {
-				org_n[i]--;
-				for (int j = 0; j < 26; j++) {
-					org_n[j]++;
-					vist = check(org_n, com_n);
-					org_n[j]--;
-					if (vist) break;
-				}
-				org_n[i]++;
-				if (vist) break;
-			}
-		}
-		else continue;
-		if (vist) {
-			ans++;
-		}
+		if (vist) ans++;
 	}
 	cout << ans;
-	return 0;
 }
