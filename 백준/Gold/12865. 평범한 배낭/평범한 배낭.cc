53be9fe7f0246a76c0a73b9a49c58d81eaf79f57
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-07-20 11:40:05 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,38 +1,24 @@
 #include <iostream>
-#include <vector>
-#include <math.h>
+#define MAX_W 100001
 using namespace std;
-
-int value[101][100001] = { 0, };
-
+int dp[MAX_W];
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	int n, w, v, k;
-
-	vector<pair<int,int>> o;
-
+	int n, k;
 	cin >> n >> k;
-
+	int weights[100];
+	int values[100];
 	for (int i = 0; i < n; i++) {
+		int w, v;
 		cin >> w >> v;
-		o.push_back(make_pair(w, v));
+		weights[i] = w;
+		values[i] = v;
 	}
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			if (o[i-1].first > j) { //배낭에 넣을 수 없는 경우.
-				value[i][j] = value[i-1][j];
-			}
-			else {
-			
-				value[i][j] = max(value[i - 1][j], value[i - 1][j - o[i-1].first] + o[i-1].second);
-			}
+	for (int i = 0; i < n; i++) {
+		for (int j = k; j >= 1; j--) {
+			if (weights[i] > j) break;
+			dp[j] = max(dp[j], dp[j - weights[i]] + values[i]);
 		}
 	}
-	
-	cout << value[n][k];
-
+	cout << dp[k];
+	return 0;
 }
