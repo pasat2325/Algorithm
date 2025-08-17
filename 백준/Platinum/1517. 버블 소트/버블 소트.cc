e91977d19ba91b7c97096114677216941d092a8b
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-08-17 13:41:52 +0900

    [Platinum V] Title: 버블 소트, Time: 424 ms, Memory: 12280 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.cc
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -6,23 +6,18 @@ int n;
 long long invcnt = 0;
 vector<int> arr;
 
-// 1'000'000
-
-vector<int> two_arr_sort(vector<int> first, vector<int> second) {
+vector<int> two_arr_sort(vector<int> f, vector<int> s) {
 	int f_p = 0; int s_p = 0;
-
-	vector<int>temp;
-
-	while (f_p < first.size() || s_p < second.size()) {
-		if (s_p >= second.size() || (f_p < first.size() && first[f_p] <= second[s_p])) {
-			temp.push_back(first[f_p]);
+	vector<int> temp;
+	while (f_p < f.size() || s_p < s.size()) {
+		if (s_p >= s.size() || (f_p < f.size() && f[f_p] <= s[s_p])) {
+			temp.push_back(f[f_p]);
 			f_p++;
 		}
 		else {
-			temp.push_back(second[s_p]);
+			temp.push_back(s[s_p]);
 			s_p++;
-
-			invcnt += first.size() - f_p;
+			invcnt += f.size() - f_p;
 		}
 	}
 	return temp;
@@ -35,19 +30,20 @@ vector<int> merge_sort(int l, int r) {
 	}
 
 	int m = (l + r) / 2;
-
 	vector<int> first = merge_sort(l, m);
 	vector<int> second = merge_sort(m + 1, r);
+
 	return two_arr_sort(first, second);
 }
 
 int main() {
 	cin >> n;
-
-	while(n--) {
+	
+	while (n--) {
 		int x; cin >> x;
 		arr.push_back(x);
 	}
 	merge_sort(0, arr.size() - 1);
 	cout << invcnt;
+
 }
