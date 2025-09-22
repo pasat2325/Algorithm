46214140d15017925c038cc7cef7624628c3e916
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-09-22 14:18:37 +0900

    [Gold III] Title: 무등산 등반, Time: 88 ms, Memory: 8200 KB -BaekjoonHub

--- 백준/Gold/34409. 무등산 등반/무등산 등반.cc
+++ 백준/Gold/34409. 무등산 등반/무등산 등반.cc
@@ -4,53 +4,60 @@
 #include <tuple>
 using namespace std;
 int main() {
-	int n, m; cin >> n >> m;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N, M; cin >> N >> M;
 	int ix, iy; cin >> ix >> iy;
 	ix--; iy--;
-	long long a, b; int c; cin >> a >> b >> c;
-	vector<vector<int>> H(n, vector<int>(m));
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			cin >> H[i][j];
+	int a, b, c; cin >> a >> b >> c;
+	vector<vector<int>>H(N, vector<int>(M));
 
-	int tph = 0;
-	int tpx = 0, tpy = 0;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++) {
+	int tpx = -1, tpy = -1, tph = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			int ih; cin >> ih;
+			H[i][j] = ih;
 			if (tph < H[i][j]) {
 				tph = H[i][j];
 				tpx = i; tpy = j;
 			}
 		}
-	const long long INF = (1LL << 60);
-	vector<vector<long long>> dist(n, vector<long long>(m, INF));
+	}
+
+	const long long INF = 5e9;
+	int dx[4] = { 1,-1,0,0 };
+	int dy[4] = { 0,0,1,-1 };
+	vector<vector<long long>> dist(N, vector<long long>(M, INF));
 	using Node = tuple<long long, int, int>;
 	priority_queue<Node, vector<Node>, greater<Node>> pq;
 
 	dist[ix][iy] = 0;
 	pq.emplace(0, ix, iy);
 
-	int dx[4] = { 1,-1,0,0 };
-	int dy[4] = { 0,0,1,-1 };
 	while (!pq.empty()) {
-		Node cur_node = pq.top();
-		long long d = get<0>(cur_node);
-		int x = get<1>(cur_node);
-		int y = get<2>(cur_node);
+		Node cur = pq.top();
+		long long d = get<0>(cur);
+		int cx = get<1>(cur);
+		int cy = get<2>(cur);
 		pq.pop();
-		if (d != dist[x][y]) continue;
-		if (x == tpx && y == tpy) {
+		
+		if (d != dist[cx][cy]) continue;
+
+		if (cx == tpx && cy == tpy) {
 			cout << d << "\n";
 			return 0;
 		}
 
-		for (int k = 0; k < 4; k++) {
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-			if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-			int dh = H[nx][ny] - H[x][y];
+		for (int i = 0; i < 4; i++) {
+			int nx = cx + dx[i];
+			int ny = cy + dy[i];
+			if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+			int dh = H[nx][ny] - H[cx][cy];
 			if (abs(dh) > c) continue;
-			long long w = (dh == 0) ? 1 : (dh > 0 ? 1LL * dh * a : 1LL * (-dh) * b);
+
+			long long w = (dh == 0) ? 1 : ((dh > 0) ? 1LL * dh * a : 1LL * (-dh) * b);
+			
 			if (dist[nx][ny] > d + w) {
 				dist[nx][ny] = d + w;
 				pq.emplace(dist[nx][ny], nx, ny);
@@ -58,4 +65,6 @@ int main() {
 		}
 	}
 	cout << -1 << "\n";
+	return 0;
+
 }
