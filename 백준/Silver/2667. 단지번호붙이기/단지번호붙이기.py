0aae42447c1ca1991b8300f81cdead5e4ebabfba
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-09-26 17:43:49 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -2,37 +2,32 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-grid = [list(map(int, input().strip()))for _ in range(n)]
-
-
-visited = [ [False for j in range(n)] for i in range(n)]
+grid = [list(map(int, input().strip())) for _ in range(n)]
+visited = [[False for j in range(n)] for i in range(n)]
 dx = [1,-1,0,0]
-dy = [0,0,1,-1]
+dy = [0, 0, 1, -1]
 
 cnt = 0
 def dfs(x, y):
-    global cnt
     visited[x][y] = True
+    global cnt
     cnt += 1
-
     for i in range(4):
-        nextx = x + dx[i]
-        nexty = y + dy[i]
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-        if nextx < 0 or nexty < 0 or nextx >= n or nexty >= n or grid[nextx][nexty] == 0 or visited[nextx][nexty] == True:
+        if nx < 0 or ny < 0 or nx >= n or ny >= n or grid[nx][ny] == 0 or visited[nx][ny] == True:
             continue
         
-        dfs(nextx, nexty)
-    
-    return
+        dfs(nx, ny)
 
 cnts = []
 for i in range(n):
     for j in range(n):
-        cnt = 0
         if grid[i][j] == 0 or visited[i][j] == True:
             continue
-        dfs(i,j)
+        cnt = 0
+        dfs(i, j)
         cnts.append(cnt)
 
 cnts.sort()
