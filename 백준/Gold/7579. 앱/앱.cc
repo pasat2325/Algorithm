d8b914718ade750f66fdfd178885684933925279
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-09-30 15:29:44 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,38 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-
 int main() {
 	int N, M; cin >> N >> M;
-	vector<int> bytes(N + 1);
-	vector<int> costs(N + 1);
+	vector<int>v(N + 1);
+	vector<pair<int, int>> V(N + 1);
 
 	for (int i = 1; i <= N; i++) {
-		cin >> bytes[i];
+		cin >> v[i];
 	}
 	int max_cost = 0;
 	for (int i = 1; i <= N; i++) {
-		cin >> costs[i];
-		max_cost += costs[i];
+		int f = v[i];
+		int s; cin >> s;
+		V[i] = { f,s };
+		max_cost += s;
 	}
 
-	vector<vector<int>> dp(N + 1, vector<int>(max_cost + 1, 0));
-
+	vector<int> dp(max_cost + 1, 0);
 	for (int i = 1; i <= N; i++) {
-		for (int j = 0; j <= max_cost; j++) {
-			if (j - costs[i] >= 0) {
-				dp[i][j] = max(dp[i][j], dp[i - 1][j - costs[i]] + bytes[i]);
+		int mi = V[i].first;
+		int ci = V[i].second;
+		for (int j = max_cost; j >= 0; j--) {
+			if (j - ci >= 0) {
+				dp[j] = max(dp[j], dp[j - ci] + mi);
 			}
-			dp[i][j] = max(dp[i - 1][j], dp[i][j]);
 		}
 	}
 
-	int ans = 0;
 	for (int i = 0; i <= max_cost; i++) {
-		if (dp[N][i] >= M) {
-			ans = i;
+		if (dp[i] >= M) {
+			cout << i << "\n";
 			break;
 		}
 	}
-	cout << ans;
+
 }
