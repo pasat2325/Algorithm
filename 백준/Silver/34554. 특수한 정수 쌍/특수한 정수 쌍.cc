c5bc99fc84172178022d91aadd226fda179507b9
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-10-14 19:51:15 +0900

    [Silver IV] Title: 특수한 정수 쌍, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/34554. 특수한 정수 쌍/특수한 정수 쌍.cc
+++ 백준/Silver/34554. 특수한 정수 쌍/특수한 정수 쌍.cc
@@ -1,25 +1,30 @@
+
 #include <iostream>
 #include <vector>
 #include <cmath>
 using namespace std;
 
-int main() {
-	int t; cin >> t;
-	vector<bool>v(10002, false);
-	v[1] = true;
-	for (int i = 2; i < 10002; i++) {
-		for (int j = 2; i * j < 10002; j++) {
-			int x = i * j;
+const int MAX = 10'001;
 
-			v[x] = true;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int t; cin >> t;
+	vector<bool> is_prime(MAX + 1, true);
+	is_prime[0] = is_prime[1] = false;
+
+	for (int i = 2; i * i <= MAX; i++) {
+		if (is_prime[i]) {
+			for (int j = i * i; j <= MAX; j += i) {
+				is_prime[j] = false;
+			}
 		}
 	}
-	
 	while (t--) {
 		int n; cin >> n;
-		if (v[n + 1] == false) {
-			cout << 1 << "\n";
-			cout << 1 << " " << n + 1 << "\n";
+		if (is_prime[n + 1]) {
+			cout << 1 << "\n" << 1 << " " << n + 1 << "\n";
 		}
 		else cout << 0 << "\n";
 	}
