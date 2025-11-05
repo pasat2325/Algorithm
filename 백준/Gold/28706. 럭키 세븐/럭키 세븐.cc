43c091ade16cf4e10d06f5bd9af60c82bd699a5a
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-11-05 14:58:39 +0900

    [Gold V] Title: 럭키 세븐, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/28706. 럭키 세븐/럭키 세븐.cc
+++ 백준/Gold/28706. 럭키 세븐/럭키 세븐.cc
@@ -2,9 +2,7 @@
 #include <cstring>
 using namespace std;
 
-const int MAX_N = 200'001;
-
-bool dp[MAX_N][7]; // N번째 턴까지 수행하였을 때, {0 ~ 6} 나머지가 존재하는지 체크 
+bool dp[2][7]; // [이전/현재][0~6 나머지] 
 
 int calc_remain(char op, int num, int r) {
 	if (op == '+') {
@@ -15,26 +13,35 @@ int calc_remain(char op, int num, int r) {
 
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
 	int T; cin >> T;
 	while (T--) {
 		int N; cin >> N;
 		char op1, op2; int num1, num2;
 
-		memset(dp, false, MAX_N * 7);
+		memset(dp, false, sizeof(dp));
 		
 		dp[0][1] = true;
 
 		for (int i = 1; i <= N; i++) {
 			cin >> op1 >> num1 >> op2 >> num2;
 
+			int current = i % 2;
+			int prev = (i - 1) % 2;
+
+			memset(dp[current], false, sizeof(dp[current]));
+
 			for (int j = 0; j < 7; j++) {
-				if (dp[i - 1][j] == true) {
-					dp[i][calc_remain(op1, num1, j)] = true;
-					dp[i][calc_remain(op2, num2, j)] = true;
+				if (dp[prev][j] == true) {
+					dp[current][calc_remain(op1, num1, j)] = true;
+					dp[current][calc_remain(op2, num2, j)] = true;
 				}
 			}
 		}
-		if (dp[N][0] == true) {
+		if (dp[N % 2][0] == true) {
 			cout << "LUCKY\n";
 		}
 		else cout << "UNLUCKY\n";
