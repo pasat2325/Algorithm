04be2abf9cc47ce8e764126a4b055789bae85643
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-11-18 12:18:10 +0900

    [Gold V] Title: 가짜소수, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/4233. 가짜소수/가짜소수.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+using ll = long long;
+const long long MAX_P = 1e9 + 1;
+
+ll modpow(ll a, ll b, ll mod) {
+	if (b == 0) return 1;
+	if (b == 1) return a % mod;
+
+	ll h = modpow(a, b / 2, mod);
+	ll r = (h * h) % mod;
+	if (b % 2 == 1) {
+		r = (r * a) % mod;
+	}
+	return r;
+}
+
+
+int main() {
+	while (true) {
+		int p, a; cin >> p >> a;
+		if (p == 0 && a == 0) break;
+
+		bool is_prime = true;
+		for (int i = 2; i <= sqrt(p); i++) {
+			if (p % i == 0) is_prime = false;
+		}
+
+		if (is_prime) {
+			cout << "no" << "\n";
+			continue;
+		}
+
+		if (modpow(a, p, p) == a % p) cout << "yes" << "\n";
+		else cout << "no" << "\n";
+	}
+}
