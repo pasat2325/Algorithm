1942c12a59d074cf9dfe4120c0aae6239b42f4bf
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-11-28 15:58:04 +0900

    [Silver IV] Title: 수 찾기, Time: 488 ms, Memory: 48600 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,28 @@
+n = int(input())
+A = list(map(int,input().split()))
+
+m = int(input())
+B = list(map(int, input().split()))
+
+def binary_search(x):
+    start = 0
+    end = len(A) - 1
+
+    while (start <= end):
+        mid = (start + end) // 2
+
+        if A[mid] == x:
+            return 1
+        
+        elif A[mid] > x:
+            end = mid - 1
+        
+        elif A[mid] < x:
+            start = mid + 1
+    
+    return 0
+
+A.sort()
+for v in B:
+    print(binary_search(v))
+
