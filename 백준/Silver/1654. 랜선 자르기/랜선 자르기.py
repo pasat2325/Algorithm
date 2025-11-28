b099880362786c39695512c58a05bb777678a6f5
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-11-28 18:07:02 +0900

    [Silver II] Title: 랜선 자르기, Time: 108 ms, Memory: 110708 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+lans = []
+
+for _ in range(n):
+    x = int(input())
+    lans.append(x)
+
+max_length = max(lans)
+
+# 랜선을 x 단위로 자를 때 몇 개의 랜선을 얻을 수 있는가?
+def lan_count(x):
+    cnt = 0
+    
+    for l in lans:
+        if l >= x:
+            cnt += l // x
+    return cnt
+
+def binary_search():
+    left = 1
+    right = max_length
+    res = 0
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if lan_count(mid) < m:
+            right = mid - 1
+        elif lan_count(mid) >= m:
+            left = mid + 1
+            res = mid
+    return res
+
+print(binary_search())
