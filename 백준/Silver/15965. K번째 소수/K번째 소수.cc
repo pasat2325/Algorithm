e89d0099d15dfa85e3fde3c7309a9088fc798dab
김은수 <60413453+pasat2325@users.noreply.github.com>
2025-12-20 13:59:52 +0900

    [Silver II] Title: K번째 소수, Time: 64 ms, Memory: 9396 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.cc
+++ 백준/Silver/15965. K번째 소수/K번째 소수.cc
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+const int MAX = 10'000'000;
 
-vector<bool> is_prime(500'001, true);
+vector<bool> is_prime(MAX, true);
 vector<int> primes;
 
 int main() {
 	is_prime[0] = false;
 	is_prime[1] = false;
 
-	for (int i = 2; i * i < 500'001; i++) {
+	for (int i = 2; i * i < MAX; i++) {
 		if (!is_prime[i]) continue;
 
-		for (int j = i * 2; j < 500'001; j += i) {
+		for (int j = i * 2; j < MAX; j += i) {
 			is_prime[j] = false; 
 		}
 	}
 	
-	for (int i = 2; i < 500'001; i++) {
+	for (int i = 2; i < MAX; i++) {
 		if (is_prime[i]) primes.push_back(i);
 	}
 
